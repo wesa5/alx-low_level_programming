6ec4c87f857c3eeddc0e580bc54444ddb95124a5
wesa5 <wobulufrancis@gmail.com>
2022-03-14 10:39:37 -0700

    a

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,60 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
-* main - Program entry point
-*
-* Return: 0 on success. Error code otherwise
+* main - Entry block
+* Description: lowercase without q and e
+* Return: 0
 */
 int main(void)
 {
-	char ch = 'a';
-		while (ch <= 'z')
+	char c = 'a';
+
+		while (c <= 'z')
 		{
-			if (ch != 'e' && ch != 'q')
+			if (c != 'q' && c != 'e')
 			{
-				putchar(ch);
+				putchar(c);
 			}
-			ch++;
+			c++;
 		}
-		putchar('\n');
 
-	return (0);
-}
-
-#include <stdio.h>
-/**
-* main - Program entry point
-*
-* Return: 0 on success. Error code otherwise
-*/
-int main(void)
-{
-	for (char ch = 'a'; ch <= 'z'; ch++)
-	{
-		if (ch != 'e' && ch != 'q')
-		{
-			putchar(ch);
-		}
-	}
 	putchar('\n');
-
 	return (0);
-}
-#include <stdio.h>
-/**
-* main - Program entry point
-*
-* Return: 0 on success. Error code otherwise
-*/
-int main(void)
-{
-	for (char ch = 'a'; ch <= 'z'; ch++)
-	{
-		if (ch == 'e' || ch == 'q')
-			ch++;
-		putchar(ch);
 
-	}
-	putchar('\n');
-
-	return (0);
 }
