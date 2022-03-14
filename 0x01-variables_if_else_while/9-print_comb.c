753f5bfe1ff13d073f76d8d03430c0665898ddb1
wesa5 <wobulufrancis@gmail.com>
2022-03-14 11:44:23 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stidio.h>
+/**
+ * main - program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
