a72779bbbad05c05c7d610b2cc3fe3d0fa062c54
wesa5 <wobulufrancis@gmail.com>
2022-03-14 11:14:44 -0700

    alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* 
+* Return: 0 on success. Error code otherwise
+*/
+int main(void)
+{
+	char i = 'z';
+
+		while (i >= 'a')
+		{
+			putchar(i);
+			i--;
+		}
+
+	putchar('\n');
+	return (0);
+}
