619fefe1e1195a292da6d9f6588bc0b336b0101f
wesa5 <99413671+wesa5@users.noreply.github.com>
2022-04-19 06:22:18 +0300

    3-main

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - entry to program
+ * @argc: argument count
+ * @argv: arrary of arguments
+ *
+ * Return: Always 0, 98 of argc error and 99 on illegal operator
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+
+	return (0);
+}
